02c2a5b9dbf3f1677f1606db32a2e31a40e118b2
Ronald McGuire <ronaldmcguire@PSADMINs-MacBook-Air-86.local>
2020-07-29 12:30:50 -0400

    Setting up the div tab to display the site

--- studio_ghibli/src/App.js
+++ studio_ghibli/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    
+    <div>
+      
+    </div>
   );
 }
 
