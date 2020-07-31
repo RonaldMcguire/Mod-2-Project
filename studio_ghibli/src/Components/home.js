5a5964a0a377d0a951de6c7703ccbf8fac500b70
Ronald McGuire <ronaldmcguire@PSADMINs-MacBook-Air-86.local>
2020-07-31 19:56:11 -0400

    res.data.results

--- studio_ghibli/src/Components/home.js
+++ studio_ghibli/src/Components/home.js
@@ -13,7 +13,7 @@ class home extends React.Component{
 }
     componentDidMount(); {
             axios.get(`${BASE_URL}`)
-            .then(res => this.ListeningStateChangedEvent({ info: res.data }))
+            .then(res => this.ListeningStateChangedEvent({ info: res.data.results }))
             .catch(e => console.error(e.message));
 
 
